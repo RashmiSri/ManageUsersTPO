228683c2819888f9d4896105819dda669c9d7ff0
RashmiSri <rashmisri1995@gmail.com>
2016-03-16 00:25:53 +0530

    Update ManageUsersService.java

--- src/org/crce/interns/service/ManageUsersService.java
+++ src/org/crce/interns/service/ManageUsersService.java
@@ -1,7 +1,10 @@
 package org.crce.interns.service;
+import java.util.ArrayList;
 import java.util.List;
+
 import org.crce.interns.dao.ManageUsersDao;
 import org.crce.interns.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,16 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("crudService")
 @Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
 public class ManageUsersService {
-	
+	@Autowired
 	private ManageUsersDao crudDao;
 	public void createDetails(User user){
+		System.out.println("Service "+user.getUserName());
 		crudDao.createDetails(user);
 		//sessionFactory.getCurrentSession().save(user);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<User> retreiveDetails(String company){
-		List<User> userList=crudDao.retreiveDetails(company);
+		List<User> userList=new ArrayList<User>();
+		userList.addAll(crudDao.retreiveDetails(company));
 		return userList;
 	//	Criteria criteria=sessionFactory.getCurrentSession().createCriteria(User.class);
 		//return criteria.list();
